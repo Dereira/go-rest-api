835bb9fc7ca4b3ea8eb5f2ef579e067ea9865c77
Daniel Pereira <daniel.pereira@ses.com>
2021-11-02 11:42:29 +0100

    Structuring the application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up the app")
+	return nil
+}
+
 func main() {
 	fmt.Printf("Go Rest API test")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up the Rest API")
+		fmt.Println(err)
+	}
 }
