bcaee1738933c1de7e440facc37e185d908e417d
Daniel Pereira <daniel.pereira@ses.com>
2021-11-02 14:40:15 +0100

    Add health check

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/Dereira/go-rest-api/internal/transport/http"
+)
 
 type App struct{}
 
 func (app *App) Run() error {
 	fmt.Println("Setting up the app")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
 	return nil
 }
 
 func main() {
-	fmt.Printf("Go Rest API test")
+	fmt.Println("Go Rest API test")
 	app := App{}
 	if err := app.Run(); err != nil {
 		fmt.Println("Error starting up the Rest API")
